cac8883b1c2844a58eea0c82e82793dcac16c1cc
Ruel <157725990+Cruelhelp@users.noreply.github.com>
2026-01-09 21:07:19 -0500

    Add files via upload

--- public/js/mobile-nav-component.js
+++ public/js/mobile-nav-component.js
@@ -88,12 +88,6 @@ function renderMobileNav(options = {}) {
                             </span>
                         </button>
                     `}
-                    <button class="mobile-nav-item" onclick="toggleMobileSidebar()">
-                        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"/>
-                        </svg>
-                        <span>Menu</span>
-                    </button>
                 </div>
             </nav>
         `;
